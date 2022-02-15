e9a700a9a8c3242fc405766c0066a12375b15a85
ktwthetop777 <99589856+ktwthetop777@users.noreply.github.com>
2022-02-16 01:48:26 +0900

    Update bitcoinautotrade.py

--- bitcoinautotrade.py
+++ bitcoinautotrade.py
@@ -37,6 +37,7 @@ upbit = pyupbit.Upbit(access, secret)
 print("autotrade start")
 
 # 자동매매 시작
+start_price = 0
 while True:
     try:
         now = datetime.datetime.now()
@@ -46,14 +47,26 @@ while True:
         if start_time < now < end_time - datetime.timedelta(seconds=10):
             target_price = get_target_price("KRW-XRP", 0.5)
             current_price = get_current_price("KRW-XRP")
-            if target_price < current_price:
+            if start_price *1.15 < current_price and start_price > 0:
+                btc = get_balance("XRP")
+                if btc > 0.00008:
+                    upbit.sell_market_order("KRW-XRP", btc*0.9995)
+                    start_price = 0
+            elif start_price * 0.9 > current_price and start_price > 0:
+                btc = get_balance("XRP")
+                if btc > 0.00008:
+                    upbit.sell_market_order("KRW-XRP", btc*0.9995)
+                    start_price = 0
+            if target_price < current_price and start_price == 0:
                 krw = get_balance("KRW")
                 if krw > 5000:
                     upbit.buy_market_order("KRW-XRP", krw*0.9995)
+                    start_price = get_current_price("KRW-XRP")
         else:
             btc = get_balance("XRP")
             if btc > 0.00008:
                 upbit.sell_market_order("KRW-XRP", btc*0.9995)
+                start_price = 0
         time.sleep(1)
     except Exception as e:
         print(e)
